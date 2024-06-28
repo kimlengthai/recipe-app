72af39c4b33086ada5928415d29f6de9a6523717
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2024-06-28 13:49:56 +0700

    get the searchParam and call the API
    Doing it by take a state and save these recipes and we have to render the list of recipes in this component.

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -4,7 +4,7 @@ import { GlobalContext } from "../../context";
 
 export default function Navbar() {
 
-    const {searchParam, setSearchParam} = useContext(GlobalContext);
+    const {searchParam, setSearchParam, handleSubmit} = useContext(GlobalContext);
 
     console.log(searchParam);
 
@@ -13,7 +13,7 @@ export default function Navbar() {
         <h2 className="text-2xl font-semibold">
           <NavLink to={'/'}>FoodRecipe</NavLink>
         </h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input 
             type="text"
             name="search"
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -3,12 +3,22 @@ import { createContext, useState } from "react";
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({children}) {
-
     const [searchParam, setSearchParam] = useState('');
-    
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`);
+            const data = await res.json();
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
-     <GlobalContext.Provider value={{searchParam, setSearchParam}}>
-     {children}
-     </GlobalContext.Provider>
-     );
+        <GlobalContext.Provider value={{ searchParam, setSearchParam, handleSubmit }}>
+            {children}
+        </GlobalContext.Provider>
+    );
 }
