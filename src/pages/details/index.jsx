96e708faaccb478567b600b1fe72ea32d31ac801
Kimleng <141606539+kimlengthai@users.noreply.github.com>
2024-06-29 21:26:53 +0700

    Render the ingredient

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -35,7 +35,7 @@ export default function Details() {
 
     return (
         <div className="container mx-auto py-10 grid grid-cols-1 lg:grid-cols-2 gap-10">
-            <div className="row-start-2 lg:row-start-auto">
+            <div className="lg:col-start-1 lg:col-end-2">
                 <div className="h-96 overflow-hidden rounded-xl group">
                     <img
                         src={recipeDetailsData?.image_url}
@@ -43,8 +43,25 @@ export default function Details() {
                         className="w-full h-full object-cover block group-hover:scale-105 duration-300"
                     />
                 </div>
+            </div>
+            <div className="lg:col-start-2 lg:col-end-3">
                 <div className="mt-5">
-                    <h1 className="text-3xl font-bold">{recipeDetailsData?.title}</h1>
+                    <h1 className="text-3xl font-bold text-black">{recipeDetailsData?.title}</h1>
+                    <div>
+                        <button className="p-3 px-8 rounded-lg text-sm uppercase font-medium tracking-wider mt-3 inline-block shadow-md bg-black text-white">Save as favorites</button>
+                    </div>
+                    <div className="mt-5">
+                        <span className="text-2xl font-semibold text-black">
+                            Ingredients:
+                        </span>
+                        <ul className="flex flex-col gap-3">
+                            {recipeDetailsData?.ingredients?.map((ingredient, index) => 
+                                <li key={index} className="text-gray-600">
+                                    {ingredient.quantity ? `${ingredient.quantity} ${ingredient.unit}` : ''} {ingredient.description}
+                                </li>
+                            )}
+                        </ul>
+                    </div>
                     <p className="text-gray-600 mt-3">Publisher: {recipeDetailsData?.publisher}</p>
                 </div>
             </div>
